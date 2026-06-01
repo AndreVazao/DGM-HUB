8f3296631e3839603315cc8c498cfa00935f974d
André Vazão <vazao.andre@gmail.com>
2026-06-01 10:47:35 +0100

    Add PowerShell execution tool

---
+++ src/dgm_hub/tools/powershell_tool.py
@@ -0,0 +1,17 @@
+from dgm_hub.execution.command_runner import CommandRunner
+from dgm_hub.tools.base import Tool
+
+
+class PowerShellTool(Tool):
+    name = "powershell"
+
+    def __init__(self):
+        self.runner = CommandRunner()
+
+    def execute(self, command: str):
+        return self.runner.run([
+            "powershell",
+            "-NoProfile",
+            "-Command",
+            command,
+        ])
